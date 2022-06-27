001a5f8f9648e48cb88a4cad9a5c8bfed0840f21
James Cababa <jamescababa@JimmysHeChefMBP.lan>
2022-06-26 21:28:03 -0500

    useeffect

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,12 +1,33 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import Pet from "./Pet";
 
 const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
 
 const SearchParams = () => {
-  const [location, setLocation] = useState("Seattle, WA");
+  const [location, setLocation] = useState("");
   const [animal, setAnimal] = useState("");
   const [breed, setBreed] = useState("");
+  const [pets, setPets] = useState([]);
   const breeds = [];
+
+  // the useEffect hook is inside the render bc now we have a closure where we can access all the hooks and variables within the component
+
+  // the [] tells this effect when to run. If its empty then it only runs once. If we put [animal] then it will render every time animal changes
+
+  useEffect(() => {
+    requestPets();
+  }, []); // eslint-disable-line react-hooks/exhaustive-dep
+
+  async function requestPets() {
+    const res = await fetch(
+      `http://pets-v2.dev-apis.com/pets?animal=${animal}&location=${location}&breed=${breed}`
+    );
+    const json = await res.json();
+
+    console.log(json);
+
+    setPets(json.pets);
+  }
 
   return (
     <div className="search-params">
@@ -55,6 +76,14 @@ const SearchParams = () => {
         </label>
         <button>Submit</button>
       </form>
+      {pets.map((pet) => (
+        <Pet
+          name={pet.name}
+          animal={pet.animal}
+          breed={pet.breed}
+          key={pet.id}
+        />
+      ))}
     </div>
   );
 };
