d5275d033d8232132edc12cf8c89ee299cc8bb16
James Cababa <jamescababa@JimmysHeChefMBP.lan>
2022-06-28 21:20:12 -0500

    custom hook complete

---
+++ src/useBreedList.js
@@ -0,0 +1,35 @@
+import { useState, useEffect } from "react";
+
+// if I select cat and get some breeds but then select dog and get some breeds and go back to cats, we don't have to make another API req to get the cat breeds. We are storing it locally here
+const localCache = {};
+
+export default function useBreedList(animal) {
+  const [breedList, setBreedList] = useState([]);
+  const [status, setStatus] = useState("unloaded"); // this string represents the state that the hook is in
+
+  useEffect(() => {
+    if (!animal) {
+      setBreedList([]);
+    } else if (localCache[animal]) {
+      setBreedList(localCache[animal]);
+    } else {
+      requestBreedList();
+    }
+    // set the setBreedList to be empty bc if we are switching back and forth between breeds, there should never be a an interim state when we are requesting data from the API where dogs have cat breeds and vice versa
+    async function requestBreedList() {
+      setBreedList([]);
+      setStatus("loading");
+
+      const res = await fetch(
+        `http://pets-v2.dev-apis.com/breeds?animal=${animal}`
+      );
+      const json = await res.json();
+      localCache[animal] = json.breeds || [];
+      setBreedList(localCache[animal]);
+      setStatus("loaded");
+    }
+    // this effect will run anytime the user selects a new animal
+  }, [animal]);
+
+  return [breedList, status];
+}
