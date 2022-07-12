9f9d940be7b64192180e2ce13c5da7c657cefa6e
James Cababa <jamescababa@JimmysHeChefMBP.lan>
2022-07-11 20:54:48 -0500

    set up router and link

---
+++ src/Details.js
@@ -0,0 +1,5 @@
+const Details = () => {
+  return <h2>lol wth</h2>;
+};
+
+export default Details;
--- src/Pet.js
+++ src/Pet.js
@@ -1,10 +1,4 @@
-// const Pet = (props) => {
-//   return React.createElement("div", {}, [
-//     React.createElement("h2", {}, props.name),
-//     React.createElement("h3", {}, props.animal),
-//     React.createElement("h3", {}, props.breed),
-//   ]);
-// };
+import { Link } from "react-router-dom";
 
 const Pet = ({ name, animal, breed, images, location, id }) => {
   // this image url is here in case we get nothing back from the api
@@ -14,7 +8,7 @@ const Pet = ({ name, animal, breed, images, location, id }) => {
   }
 
   return (
-    <a href={`/details/${id}`} className="pet">
+    <Link to={`/details/${id}`} className="pet">
       <div className="image-container">
         <img src={hero} alt={name} />
       </div>
@@ -22,7 +16,7 @@ const Pet = ({ name, animal, breed, images, location, id }) => {
         <h1>{name}</h1>
         <h2>{`${animal} - ${breed} - ${location}`}</h2>
       </div>
-    </a>
+    </Link>
   );
 };
 export default Pet;
