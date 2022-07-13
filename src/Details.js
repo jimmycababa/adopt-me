05ba45c82800af42f376b8973bc04c6d6319e603
James Cababa <jamescababa@JimmysHeChefMBP.lan>
2022-07-12 22:33:35 -0500

    class component

--- src/Details.js
+++ src/Details.js
@@ -1,5 +1,42 @@
-const Details = () => {
-  return <h2>lol wth</h2>;
-};
+import { Component } from "react";
+import { withRouter } from "react-router-dom";
 
-export default Details;
+// class components always has a render method that returns something
+class Details extends Component {
+  constructor() {
+    super();
+
+    this.state = { loading: true };
+  }
+  async componentDidMount() {
+    const res = await fetch(
+      `http://pets-v2.dev-apis.com/pets?id=${this.props.match.params.id}`
+    );
+    const json = await res.json();
+    this.setState(
+      Object.assign(
+        {
+          loading: false,
+        },
+        json.pets[0]
+      )
+    );
+  }
+
+  render() {
+    const { animal, breed, city, state, description, name } = this.state;
+    return (
+      <div className="details">
+        <div>
+          <h1>{name}</h1>
+          <h2>{`${animal} - ${breed} - ${city} - ${state}`}</h2>
+          <button>Adopt {name}</button>
+          <p>{description}</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+// withRouter will inject on the react router information into the route(passing the props in)
+export default withRouter(Details);
