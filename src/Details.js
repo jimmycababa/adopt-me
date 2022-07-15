95b796a2c2fc00d6ea177591ab6a3d82ceb1327f
James Cababa <jamescababa@JimmysHeChefMBP.lan>
2022-07-14 21:39:49 -0500

    class components

--- src/Details.js
+++ src/Details.js
@@ -3,11 +3,8 @@ import { withRouter } from "react-router-dom";
 
 // class components always has a render method that returns something
 class Details extends Component {
-  constructor() {
-    super();
+  state = { loading: true };
 
-    this.state = { loading: true };
-  }
   async componentDidMount() {
     const res = await fetch(
       `http://pets-v2.dev-apis.com/pets?id=${this.props.match.params.id}`
@@ -24,6 +21,10 @@ class Details extends Component {
   }
 
   render() {
+    if (this.state.loading) {
+      return <h2>loading ...</h2>;
+    }
+
     const { animal, breed, city, state, description, name } = this.state;
     return (
       <div className="details">
